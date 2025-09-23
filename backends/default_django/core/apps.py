e3b14fb5b1e92ee31323032f1065296153e4c74d
Matrix Jef <jefcolbi@gmail.com>
2025-09-23 19:11:02 +0100

    ignore programming error when creating candidate parties

--- backends/default_django/core/apps.py
+++ backends/default_django/core/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.db.utils import ProgrammingError
 
 
 class CoreConfig(AppConfig):
@@ -7,15 +8,17 @@ class CoreConfig(AppConfig):
 
     def ready(self):
         from core.models import CandidateParty
+        try:
+            CandidateParty.objects.get_or_create(
+                identifier="**undecided**",
+                party_name="UNDECIDED",
+                candidate_name="UNDECIDED",
+            )
 
-        CandidateParty.objects.get_or_create(
-            identifier="**undecided**",
-            party_name="UNDECIDED",
-            candidate_name="UNDECIDED",
-        )
-
-        CandidateParty.objects.get_or_create(
-            identifier="**white**",
-            party_name="WHITE",
-            candidate_name="WHITE",
-        )
+            CandidateParty.objects.get_or_create(
+                identifier="**white**",
+                party_name="WHITE",
+                candidate_name="WHITE",
+            )
+        except ProgrammingError:
+            pass
